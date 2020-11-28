2db31dce33bf356c5aedc84719c6a62632503327
freckie <freckie@frec.kr>
2020-11-28 21:09:53 +0900

    Update: now print logs with the time

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 import json
+import uuid
 import random
 import serial
+import logging
 import threading
 import paho.mqtt.client as mqtt
 
+# CONST VALUES
+SERIAL_PORT = '/dev/ttyS0'
+SERIAL_BAUDRATE = 115200
+SERIAL_TIMEOUT = 100
+MQTT_HOST = 'localhost'
+MQTT_PORT = 1883
+
 DESTINATION = '427'
 sclient = None
-exit_thread = False
+logger = init_logger()
+
+def init_logger():
+    '''
+    Initiate main logger.
+    '''
+    _logger = logging.getLogger('Main')
+    _logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter(fmt="[ %(asctime)s ] %(message)s")
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+    _logger.addHandler(stream_handler)
+    return _logger
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print('MQTT Connected Successfully')
+        logger.info('MQTT Connected Successfully (to {}:{})'.format(MQTT_HOST, MQTT_PORT))
     else:
-        print('MQTT Connection Failed!')
+        logger.info('MQTT Connection Failed!')
         
 def on_message(client, userdata, msg):
         data = json.loads(msg.payload.decode("utf-8"))
-        print(data)
         
-        code = 0
+        code: int
         if msg.topic == '/beverage/location':
             code = handle_beverage(loc=data['location'], error_padding=5)
         elif msg.topic == '/room':
@@ -27,14 +48,16 @@ def on_message(client, userdata, msg):
             code = handle_qr()
         else:
             return
-        print('code:', code)
+        logger.info('code:', code)
         
         if code == 3:
             client.unsubscribe('/beverage/location')
+            logger.info('MQTT unsubscribed topic "/beverage/location".')
         elif code == 4:
             client.unsubscribe('/room')
+            logger.info('MQTT unsubscribed topic "/room".')
         
-def handle_beverage(loc, center=320, error_padding=10):
+def handle_beverage(loc, center=320, error_padding=5):
     x_range = (center - error_padding, center + error_padding)
     code = 0
     if loc < x_range[0]:
@@ -57,45 +80,26 @@ def handle_qr():
     sclient.write(serial.to_bytes([0x05]))
     return 5
 
-def read_serial(ser):
-    global exit_thread
-    
-    line = []
-    print('read_serial Thread started.')
-    while True:
-        print('a')
-        if exit_thread:
-            print('now breaking the loop of read_serial Thread')
-            break
-        #for c in ser.read():
-            #print(c)
-        print(ser.read(4))
-
 if __name__ == '__main__':
     # Serial Connection
-    sclient = serial.Serial(port='/dev/ttyS0', baudrate=115200, timeout=100)
+    sclient = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_BAUDRATE, timeout=SERIAL_TIMEOUT)
     if not sclient.isOpen():
-        print('Cannot open the serial port.')
-    #thr = threading.Thread(target=read_serial, args=(sclient,))
-    #thr.start()
+        logger.info('Cannot open the serial port {} .'.format(SERIAL_PORT))
     
     # MQTT Connection
-    client_id = f'python-mqtt-{random.randint(0, 1000)}'
-    client = mqtt.Client(client_id)
+    client = mqtt.Client(str(uuid.uuid1()))
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect('localhost', 1883)
-
+    client.connect(MQTT_HOST, MQTT_PORT)
 
     # MQTT Loop
     client.subscribe('/beverage/location', 1)
     client.subscribe('/room', 1)
     client.subscribe('/qr', 1)
+    client.subscribe('/supervisor', 2)
     try:
         client.loop_forever()
     except KeyboardInterrupt:
-        print('KeyboardInterrupt occured.')
+        logging.info('KeyboardInterrupt occured.')
         client.disconnect()
-        exit_thread = True
         sclient.close()
-        #thr.join()
